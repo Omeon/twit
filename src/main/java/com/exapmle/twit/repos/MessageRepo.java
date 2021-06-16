ddf519c44e3195e7cdf73f367a31750c5d24e3c5
Velind222 <foshin99@bk.ru>
2021-06-16 22:07:50 +0300

    Подключил фильтр, полностью закончил второй урок

--- src/main/java/com/exapmle/twit/repos/MessageRepo.java
+++ src/main/java/com/exapmle/twit/repos/MessageRepo.java
@@ -3,6 +3,10 @@ package com.exapmle.twit.repos;
 import com.exapmle.twit.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 
 }
