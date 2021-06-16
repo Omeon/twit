5ff1ad851fba6b2b3cc121c9a291a02096923e8e
Velind222 <foshin99@bk.ru>
2021-06-17 00:46:51 +0300

    Прикрутил регистрацию и закончил третий урок

---
+++ src/main/java/com/exapmle/twit/domain/Role.java
@@ -0,0 +1,5 @@
+package com.exapmle.twit.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/exapmle/twit/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.exapmle.twit.repos;
+
+import com.exapmle.twit.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
